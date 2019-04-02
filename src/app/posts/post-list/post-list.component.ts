fc223e5db6739ec1d1a7c644afc81d74e0e67fbd
JosephPR <reilly4181@gmail.com>
2019-04-02 11:06:23 -0400

    MEAN

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -24,12 +24,17 @@ export class PostListComponent implements OnInit, OnDestroy {
    constructor(public postsService: PostsService) {}
 
 ngOnInit() {
-  this.posts = this.postsService.getPosts();
+  this.postsService.getPosts();
   this.postsSub = this.postsService.getPostUpdateListener()
   .subscribe((posts: Post[]) => {
     this.posts = posts;
   });
   }
+
+onDelete(postId: string) {
+  this.postsService.deletePost(postId)
+}
+
   ngOnDestroy() {
     this.postsSub.unsubscribe()
   }
