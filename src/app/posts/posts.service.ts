d06374997a2c59cba4d8eb365e44cf1c28f6bbb6
JosephPR <reilly4181@gmail.com>
2019-04-10 00:06:25 -0400

    RouterLink [binding ngModel], editing posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -31,8 +31,15 @@ this.http
   this.postsUpdated.next([...this.posts]);
 });
 }
+
 getPostUpdateListener() {
   return this.postsUpdated.asObservable();
+}
+
+getPost(id: string) {
+  return this.http.get<{_id: string; title: string; content: string }>
+  ('http://localhost:3000/api/posts/' + id
+);
 }
 
 addPost(title: string, content: string) {
@@ -46,6 +53,20 @@ addPost(title: string, content: string) {
   });
 
  }
+
+updatePost(id: string, title: string, content: string) {
+  const post: Post = { id: id, title: title, content: content};
+  this.http.put('http://localhost:3000/api/posts/' + id, post)
+  .subscribe(response =>{
+    const updatedPosts  = [...this.posts];
+    const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+    updatedPosts[oldPostIndex] = post;
+    this.posts = updatedPosts;
+    this.postsUpdated.next([...this.posts]);
+  });
+}
+
+
 deletePost(postId: string) {
   this.http.delete('http://localhost:3000/api/posts/' + postId)
   .subscribe(() => {
